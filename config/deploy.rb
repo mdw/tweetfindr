aaaf43c1fe4471e071fbeef3f5f7958085e3d932
Mike Ward <github@mikeward.net>
2009-03-27 02:53:04 -0400

    fixed broken link

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,11 +3,11 @@ set :deploy_to, "/var/www/#{application}"
 set :user, "vending"
 
 # scm settings
-#set :scm, :git
-#set :repository,  "git@boca.unfuddle.com:boca/vending.git"
-set :repository, "http://boca.unfuddle.com/svn/boca_vend/trunk"
-set :scm_user, "cap"
-set :scm_pw, "cap1fy"
+set :scm, :git
+set :repository,  "git@github.com:mdw/twitvend.git"
+#set :repository, "http://boca.unfuddle.com/svn/boca_vend/trunk"
+#set :scm_user, "cap"
+#set :scm_pw, "cap1fy"
 
 role :app, "pizzavendingmachines.com"
 role :web, "pizzavendingmachines.com"
@@ -15,16 +15,18 @@ role :db, "pizzavendingmachines.com", :primary => true
 
 namespace :deploy do
 	task :update_code do
-		run "svn -q --username #{scm_user} --password #{scm_pw} --force export #{repository} #{deploy_to}"
+		run "git fetch github master"
+		#run "svn -q --username #{scm_user} --password #{scm_pw} --force export #{repository} #{deploy_to}"
 	end
 	task :symlinks do
-		system "echo 'symlinks not needed'"
+		# 
 	end
 	task :restart do
-		system "echo 'restart not needed'"
+		# 
 	end
 	task :start do
-		run "ruby #{deploy_to}/vend.rb -e production &"
+		#run "shotgun --server=thin --port=4567 #{deploy_to}/vend.rb &"
+		#run "ruby #{deploy_to}/vend.rb -e production &"
 	end
 end
 
