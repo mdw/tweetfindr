e16023645ae478fbeba8641b145e9932105b4d71
Mike Ward <github@mikeward.net>
2009-03-31 16:43:18 -0400

    changed to dedicated domain name

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,32 +1,35 @@
-set :application, "pizzavendingmachines.com"
+# application settings
+set :application, "tweetfindr.com"
+role :app, "209.123.234.193"
+role :web, "209.123.234.193"
+role :db, "tweetfindr.com", :primary => true
+
 set :deploy_to, "/var/www/#{application}"
-set :user, "vending"
+set :user, "findr"
+set :keep_releases, 6
+default_run_options[:pty] = true
 
-# scm settings
+# version control
 set :scm, :git
-set :repository,  "git@github.com:mdw/twitvend.git"
-#set :repository, "http://boca.unfuddle.com/svn/boca_vend/trunk"
-#set :scm_user, "cap"
-#set :scm_pw, "cap1fy"
+set :repository,  "git://github.com/mdw/twitvend.git"
+set :branch, "master"
+set :checkout, "export"
+set :git_shallow_clone, 1
 
-role :app, "pizzavendingmachines.com"
-role :web, "pizzavendingmachines.com"
-role :db, "pizzavendingmachines.com", :primary => true
 
 namespace :deploy do
-	task :update_code do
-		run "git fetch github master"
-		#run "svn -q --username #{scm_user} --password #{scm_pw} --force export #{repository} #{deploy_to}"
-	end
-	task :symlinks do
-		# 
-	end
+   task :update_code do
+ #     targetdir = Time.now.strftime("%Y%m%d%H%M%S")
+ #     releasedir = "#{deploy_to}/releases/#{targetdir}"
+ #     run "mkdir #{releasedir}"
+      run "git clone -q #{repository} #{release_path}"
+   end
+   task :symlink do
+      run "ln -nfs #{release_path} #{deploy_to}/current"
+   end
 	task :restart do
-		# 
-	end
-	task :start do
-		#run "shotgun --server=thin --port=4567 #{deploy_to}/vend.rb &"
-		#run "ruby #{deploy_to}/vend.rb -e production &"
+		#run "/usr/local/bin/shotgun --server=thin --port=4567 #{current_path}/vend.rb &"
+		run "ruby #{deploy_to}/vend.rb -e production &"
 	end
 end
 
--- vend.rb
+++ vend.rb
@@ -2,8 +2,6 @@
 %w(rubygems sinatra haml rfeedparser).each { |gem| require gem }
 
 configure do
-	SourceUrl = ' &nbsp; (<a href="http://github.com/mdw/twitvend/master">the source</a>)'
-	Credits = 'Powered by <a href=/sinatra>Sinatra</a> and <a href=/twitter>Twitter</a> &nbsp; (' + SourceUrl
 	TwitterSearchURL = "http://search.twitter.com/search.atom?q="
 end
 
@@ -14,7 +12,7 @@ end
 
 get '/:tag' do |t|
 	headers 'Content-Type' => 'text/html; charset=utf-8'
-  @searchchar = t =~ /^\@/ ? '%3A' : '%23'  # %23 is hash # symbol, %3A is the @ symbol
+   @searchchar = t =~ /^\@/ ? '%3A' : '%23'  # %23 is hash # symbol, %3A is the @ symbol
 	res = FeedParser.parse(TwitterSearchURL + @searchchar + t)
 	@entries = res.entries
 	haml :show
