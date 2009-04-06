4f4d4cb7e8bc47e682bcb8ed84df1a95dc98c42f
Mike Ward <github@mikeward.net>
2009-04-06 03:35:55 -0400

    added helper methods

--- findr.rb
+++ findr.rb
@@ -2,8 +2,19 @@
 %w(rubygems sinatra twitter_search haml).each { |r| require r }
 
 configure do
-   Debug = true
-   SysMessage = ""
+   @debug = true
+   @sysmessage = ""
+end
+
+helpers do
+   def termlink(hashtag)
+      url = "http://" + request.host + '/' + hashtag
+      '<a href="' + url + '" class="hash" title="search for #' + hashtag + '">#' + hashtag + '</a>'
+   end
+   def peoplelink(username)
+      url = "http://" + request.host + '/@' + username
+      '<a href="' + url + '" class="people" title="search for @' + username + '">@' + username + '</a>'
+   end
 end
 
 class Findr < Sinatra::Application
@@ -16,12 +27,17 @@ class Findr < Sinatra::Application
       halt 401, 'go away!'
    end
 
+   get '/tweetfindr' do
+      @sysmessage = 'yeah baby'
+      pass
+   end
+   
    get '/:tag' do |t|
       # add hash tag manually, since can't be passed in with URL
       @searchstr = t =~ /^@/ ? t : '#' + t
       @client = TwitterSearch::Client.new 'tweetfindr v0.2'
       @tweets = @client.query :q => @searchstr, :rpp => '25'
-      ## @tweets.each { |t| puts t.source } if Debug
+      #@tweets.each { |t| puts t.source } if @debug
       headers 'Content-Type' => 'text/html; charset=utf-8'
 	   haml :showtweets
    end
