2b8c9d9634c6e17c59c76cea422b396dc2d40812
Mike Ward <github@mikeward.net>
2009-04-11 15:17:37 -0400

    begin using database to store queries

--- findr.rb
+++ findr.rb
@@ -1,26 +1,55 @@
 # findr.rb - another useless Twitter search app
-%w(rubygems sinatra twitter_search haml).each { |r| require r }
+%w(rubygems sinatra activerecord twitter_search haml).each { |r| require r }
+%w( database helpers ).each { |lib| require "lib/#{lib}.rb" }
 
 configure do
-   @debug = true
    @sysmessage = ""
+   ActiveRecord::Base.establish_connection(
+      :adapter => "mysql",
+      :database => "tweetfindr_dev",
+      :username => "tweetfinder",
+      :password => "tf1ndr"
+   )
+   
+   begin
+      ActiveRecord::Schema.define do
+         create_table :searches do |t|
+            t.string :sstring
+            t.integer :howmany
+            t.timestamps
+         end
+      end
+   rescue ActiveRecord::StatementInvalid
+      # schema already exists
+   end
 end
 
-helpers do
-   def termlink(hashtag)
-      url = "http://" + request.host + '/' + hashtag
-      '<a href="' + url + '" class="hash" title="search for #' + hashtag + '">#' + hashtag + '</a>'
-   end
-   def peoplelink(username)
-      url = "http://" + request.host + '/@' + username
-      '<a href="' + url + '" class="people" title="search for @' + username + '">@' + username + '</a>'
+
+class Search < ActiveRecord::Base
+   validates_uniqueness_of :sstring
+   attr_accessor :howmany
+
+   def increment(id)
+      #s = find(:id)
+      #@count = s.howmany.to_i + 1
+      #s.update_attribute(:howmany, @count)
    end
 end
+
 
 class Findr < Sinatra::Application
    
+   helpers do
+      include Helpers  # all my helpers defined in helpers.rb
+      #
+      # def link_to(url, title, klass, inner)
+      # def termlink(hashtag)
+      # def peoplelink(username)
+      #
+   end
+
    get '/' do
-	   haml :howto
+      haml :howto
    end
 
    get '/:tag', :agent => /Googlebot|Slurp/ do
@@ -35,10 +64,26 @@ class Findr < Sinatra::Application
    get '/:tag/?' do |t|
       # add hash tag manually, since can't be passed in with URL
       @searchstr = t =~ /^@/ ? t : '#' + t
+
+      # save search string to database
+      @s = Search.find_by_sstring(params[:tag])
+      if !@s.nil?
+         @s.increment(@s.id)
+      else
+         search = Search.new(:sstring => t, :howmany => 1)
+         if search.save
+            status(201)
+            @sysmessage = "(saved #{search.sstring} to the database)"
+         else
+            status(412)
+            "Error: dup;licate search term, should have incremented counter\n"
+         end
+      end
+
+      # get twitter search results
       @client = TwitterSearch::Client.new 'tweetfindr v0.2'
       @tweets = @client.query :q => @searchstr, :rpp => '25'
-      #@tweets.each { |t| puts t.source } if @debug
       headers 'Content-Type' => 'text/html; charset=utf-8'
-	   haml :showtweets
+      haml :showtweets
    end
 end
