1f625463176885b484fc21fb0f21891b0241def9
Mike Ward <github@mikeward.net>
2009-04-14 14:56:46 -0400

    fixed people link problem

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,20 +2,23 @@
 
 module Helpers
 
-   def link_to(url, title, klass, inner)
-      '<a href="' + url + '" title="' + title + '" class="' + klass + '">' + inner + '</a>'
-   end
+     def link_to(url, title, klass, inner)
+        '<a href="' + url + '" title="' + title + '" class="' + klass + '">' + inner + '</a>'
+     end
 
-   def termlink(hashtag)
-      url = "http://" + request.host + '/' + hashtag
-      title = 'search for #' + hashtag
-      inner = '#' + hashtag
-      link_to(url, title, "hash", '#'+hashtag)
-   end
+     def termlink(hashtag)
+        url = "http://" + request.host + '/' + hashtag
+        title = 'search for #' + hashtag
+        link_to(url, title, "hash", '#'+hashtag)
+     end
 
-   def peoplelink(username)
-      url = "http://" + request.host + '/@' + username
-      title = 'search for @' + username
-      link_to(url, title, "people", '@'+username)
-   end
+     def peoplelink(username)
+        # why does the request.host fail to have portnum here when it works in above method?
+        #url = "http://" + request.host + '/@' + username 
+        url = "http://" + request.host
+        url << ":#{request.port}" unless request.port.nil?
+        url << "/@#{username}"
+        title = 'search for @' + username
+        link_to(url, title, "people", '@'+username)
+     end
 end
