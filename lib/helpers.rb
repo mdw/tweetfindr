21e73c3c5ae2b092eb1fa03c442dc62f20b3aa4b
Mike Ward <github@mikeward.net>
2009-04-15 01:41:48 -0400

    gave up on generating full urls

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -3,21 +3,17 @@
 module Helpers
 
      def link_to(url, title, klass, inner)
-        '<a href="' + url + '" title="' + title + '" class="' + klass + '">' + inner + "</a>"
+        '<a href="' + url + '" title="' + title + '" class="' + klass + '">' + inner + '</a>'
      end
 
      def termlink(hashtag)
-        url = "http://" + request.host + '/' + hashtag
+        url = "/#{hashtag}"
         title = 'search for #' + hashtag
         link_to(url, title, "hash", '#'+hashtag)
      end
 
      def peoplelink(username)
-        if request.host == 'localhost'
-           url = "http://" + request.host + ":#{request.port}/@#{username}"
-        else
-           url = "http://tweetfindr.com/@#{username}"
-        end
+        url = "/@#{username}"
         title = 'search for @' + username 
         link_to(url, title, "people", '@'+username)
      end
